5d129bf71a253a542a9a135e416e078e0a5180a9
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-08-26 22:13:50 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,16 @@
 # Create your costume_stores migration here
-class CreateCostumeStore < ActiveRecord::Migration 
-  
+class CreateCostumeStore < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.text :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      
+
+    end
+
+  end
+
 end

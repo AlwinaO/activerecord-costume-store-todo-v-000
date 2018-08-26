d7d8c253fa217b0218c3d1cf0fdea0e224789d69
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-08-26 22:42:59 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.text :location
+      t.string :theme
+      t.floate :price
+
+    end
+
+  end
+
+end

2e11ec8b4a20c122d765b4a89156d335f6a03855
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-08-26 22:45:44 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,6 +7,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.text :location
       t.string :theme
       t.floate :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
 
     end
 

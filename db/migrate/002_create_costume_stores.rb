94fbc91bb3a105365e9779e5aef81166134ced68
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-08-26 22:27:21 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,9 @@ class CreateCostumeStore < ActiveRecord::Migration[4.2]
       t.text :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      
+      t.boolean :still_in_business
+      t.time :opening_time
+      t.time :closing_time
 
     end
 
